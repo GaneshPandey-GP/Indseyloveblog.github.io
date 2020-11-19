94925630b11cbf7b488f78c985e7ba79cc98ec94
Tekraj Joshi <tekrajjoshi07@gmail.com>
2020-11-19 12:31:40 +0530

    react app creation

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BaseRouter from './routes';
+
+function App() {
+  return (
+    <div className="App">
+      <BaseRouter />
+    </div>
+  );
+}
+
+export default App;
