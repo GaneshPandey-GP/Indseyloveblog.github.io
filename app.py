a90421230e8eb0f4a9ba5a8e4b88e40d22c239cf
AllPlayer <ayushparida999@gmail.com>
2020-11-19 11:55:38 +0530

    fetch users api

--- app.py
+++ app.py
@@ -9,28 +9,16 @@ from pymongo import MongoClient
 ##    "database": "hostelManagementSystem",
 ##    "collection": "users",
 ##    "Document": {
-##        "name": "TestF TestL",
-##        "email": "testemail@test.com",
-##        "phone": "987543210",
-##        "fPhone": "1234567890",
-##        "aadharNo": 111111111111,
-##        "faadharNo": 222222222222,
-##        "fname": "fTestF fTestL",
-##        "college": "Test College Name",
-##        "address": "Test Address name",
-##        "price": 12000,
-##        "security": 1000,
-##        "startdate": "2020-07-13T00:00:00.000+00:00",
-##        "isActive": 1,
-##        "assigned": [1,2,1,18]
+##        
 ##    }
 ##}
 class MongoAPI:
     def __init__(self, data):
-        self.client = MongoClient("mongodb://localhost:27017/")
+        self.client = MongoClient("mongodb+srv://allplayertest:qwerty1234@examinationsystem.sqwfq.mongodb.net/Examinatipn?retryWrites=true&w=majority")
         database = data['database']
         collection = data['collection']
-        cursor = self.client[database]
+        cursor = self.client.get_database(database)
+        print(cursor)
         self.collection = cursor[collection]
         self.data = data
     def write(self, data):
@@ -40,16 +28,6 @@ class MongoAPI:
                   'Document_ID': str(response.inserted_id)}
         return output
     def read(self):
-        print(self.data)
-        documents = self.collection.find()
-        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
-        return output
-    def aadharCheck(self):
-        print(self.data)
-        documents = self.collection.find({"aadharNo":self.data['aadharNo']})
-        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
-        return output
-    def getRooms(self):
         print(self.data)
         documents = self.collection.find()
         output = [{item: data[item] for item in data if item != '_id'} for data in documents]
@@ -69,32 +47,6 @@ def register():
                         mimetype='application/json')
     obj1 = MongoAPI(data)
     response = obj1.write(data)
-    return Response(response=json.dumps(response),
-                    status=200,
-                    mimetype='application/json')
-@app.route('/aadharCheck', methods=['POST'])
-def aadhar_check():
-    data = request.json
-    if data is None or data == {}:
-        print("No data")
-        return Response(response=json.dumps({"Error": "Please provide connection information"}),
-                        status=400,
-                        mimetype='application/json')
-    obj1 = MongoAPI(data)
-    response = obj1.aadharCheck()
-    return Response(response=json.dumps(response),
-                    status=200,
-                    mimetype='application/json')
-@app.route('/getRooms', methods=['POST'])
-def get_rooms():
-    data = request.json
-    if data is None or data == {}:
-        print("No data")
-        return Response(response=json.dumps({"Error": "Please provide connection information"}),
-                        status=400,
-                        mimetype='application/json')
-    obj1 = MongoAPI(data)
-    response = obj1.getRooms()
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
