2ab38d8cf127aaec6196f47018ce12fa8de900a7
GaneshPandey-GP <ganeshpandey897@gmail.com>
2020-11-23 23:20:41 +0530

    student-dashboard

---
+++ frontend/src/components/StudentDashboard/PastsTest.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button } from '@material-ui/core';
+
+export default function PastsTests() {
+
+  return (
+    <>
+      <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Test Name</th>
+      <th scope="col">Subjects</th>
+      <th scope="col">Ends On</th>
+      <th scope="col">Start Test</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">CPP Basic</th>
+      <td>CPP</td>
+      <td>2021-12-17</td>
+      <td><Button  variant="contained" color="primary">Start</Button></td>
+    </tr>
+    <tr>
+    <th scope="row">CPP Basic</th>
+      <td>CPP</td>
+      <td>2021-12-17</td>
+      <td><Button  variant="contained" color="primary">Start</Button></td>
+    </tr>
+   
+  </tbody>
+</table>
+    </>
+  );
+}
