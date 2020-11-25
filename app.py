a2ab21ce2c4a45cf4e9d769c63da424007d4e2b2
AllPlayer <ayushparida999@gmail.com>
2020-11-25 15:07:28 +0530

    'auto-increment'

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ class MongoAPI:
         database = data['database']
         collection = data['collection']
         cursor = self.client.get_database(database)
-        print(cursor)
         self.collection = cursor[collection]
         self.data = data
     def write(self, data):
@@ -35,26 +34,21 @@ class MongoAPI:
                   'Document_ID': str(response.inserted_id)}
         return output
     def read(self):
-        print(self.data)
         documents = self.collection.find()
         output = [{item: data[item] for item in data if item != '_id'} for data in documents]
         return output
     def readWithFilter(self):
         filt = self.data['Filter']
-        print(self.data)
         documents = self.collection.find(filt)
         output = [{item: data[item] for item in data if item != '_id'} for data in documents]
         return output
     def readUsers(self):
-        print(self.data)
         documents = self.collection.find()
         output = [{item: data[item] for item in data if item != '_id'} for data in documents]
         return output
     def login(self):
-        print(self.data)
         documents = self.collection.find({"email": self.data['username'],"password":self.data['password'],"isActive":1},{'createdBy':0,'createdOn':0,'isActive':0,'password':0,'email':0,'contact':0,'fname':0,'lname':0})
         output = [{item: data[item] for item in data if item != '_id'} for data in documents]
-        print(output)
         return output
     def update(self):
         filt = self.data['Filter']
@@ -62,16 +56,29 @@ class MongoAPI:
         response = self.collection.update_one(filt, updated_data)
         output = {'Status': '1' if response.modified_count > 0 else "Nothing was updated."}
         return output
+    def getSequences(self):
+        documents = self.collection.find()
+        output = [{item: data[item] for item in data if item != '_id'} for data in documents]
+        return output
 app = Flask(__name__)
 @app.route('/register', methods=['POST'])
 def register():
     data = request.json
+    data2=json.loads('{"database":"ExaminationSystem","collection":"sequences"}')
+    obj2 = MongoAPI(data2)
+    print(obj2.getSequences()[0].get('userSequence'))
+    cid=(obj2.getSequences()[0].get('userSequence'))
+    print(cid)
     if data is None or data == {}:
         return Response(response=json.dumps({"Error": "Please provide connection information"}),
                         status=400,
                         mimetype='application/json')
     obj1 = MongoAPI(data)
     response = obj1.write(data)
+    cid2=cid+1
+    data3=json.loads('{"database":"ExaminationSystem","collection":"sequences","Filter":{"userSequence":'+str(cid)+'},"DataToBeUpdated":{"userSequence":'+str(cid2)+'}}')
+    obj3 = MongoAPI(data3)
+    obj3.update()
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
@@ -102,24 +109,44 @@ def login_class():
 @app.route('/createSubject', methods=['POST'])
 def create_subject():
     data = request.json
+    data2=json.loads('{"database":"ExaminationSystem","collection":"sequences"}')
+    obj2 = MongoAPI(data2)
+    print(obj2.getSequences()[0].get('subjectSequence'))
+    cid=(obj2.getSequences()[0].get('subjectSequence'))
+    print(cid)
+    data['document']['subid']=cid
     if data is None or data == {}:
         return Response(response=json.dumps({"Error": "Please provide connection information"}),
                         status=400,
                         mimetype='application/json')
     obj1 = MongoAPI(data)
     response = obj1.write(data)
+    cid2=cid+1
+    data3=json.loads('{"database":"ExaminationSystem","collection":"sequences","Filter":{"subjectSequence":'+str(cid)+'},"DataToBeUpdated":{"subjectSequence":'+str(cid2)+'}}')
+    obj3 = MongoAPI(data3)
+    obj3.update()
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
+    
 @app.route('/createTest', methods=['POST'])
 def create_test():
     data = request.json
+    data2=json.loads('{"database":"ExaminationSystem","collection":"sequences"}')
+    obj2 = MongoAPI(data2)
+    print(obj2.getSequences()[0].get('testSequence'))
+    cid=(obj2.getSequences()[0].get('testSequence'))
+    print(cid)
     if data is None or data == {}:
         return Response(response=json.dumps({"Error": "Please provide connection information"}),
                         status=400,
                         mimetype='application/json')
     obj1 = MongoAPI(data)
     response = obj1.write(data)
+    cid2=cid+1
+    data3=json.loads('{"database":"ExaminationSystem","collection":"sequences","Filter":{"testSequence":'+str(cid)+'},"DataToBeUpdated":{"testSequence":'+str(cid2)+'}}')
+    obj3 = MongoAPI(data3)
+    obj3.update()
     return Response(response=json.dumps(response),
                     status=200,
                     mimetype='application/json')
