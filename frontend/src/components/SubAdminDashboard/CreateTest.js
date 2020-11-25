49fb89a1f9b925c6ec20c874cef8da09b52dcb60
Tek Raj Joshi <58837959+T3KRAJ@users.noreply.github.com>
2020-11-25 21:14:47 +0530

    Update CreateTest.js

--- frontend/src/components/SubAdminDashboard/CreateTest.js
+++ frontend/src/components/SubAdminDashboard/CreateTest.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -12,44 +12,41 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import { ListItem, ListItemIcon, ListItemText, TextField } from '@material-ui/core';
 import { useAuthState } from '../../context';
-import { getSubjects } from '../../context/actions';
 import EditIcon from '@material-ui/icons/Edit';
 
 const useStyles = makeStyles((theme) => ({
   container: {
     display: 'flex',
+    minwidth: 400,
     flexWrap: 'wrap',
   },
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 120,
+    minWidth: 200,
   },
 }));
 
-export default function TestForm() {
-  const [formData, setFormData] = useState({
-    testname: "",
-    subject: "",
-  });
+export default function CreateTest() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [subject, setSubject] = React.useState('');
   const [{subjects}, dispatch] = useAuthState()
+  const [testName, setTestName] = useState('')
+  const [subjectId, setSubjectId] = useState('')
 
-  useEffect(() => {
-      getSubjects(dispatch)
-  }, [dispatch])
-
-  const handleChange = (e) => {
-    setSubject(e.target.value || '');
+  const handleInputChange = (e) => {
+    setSubject(e.target.value || '')
   };
 
-  const handleInputChange = (e) =>
-  setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleTestNameChange = (e) =>
+    setTestName(e.target.value);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    setSubjectId(subject)
+    console.log(testName, subjectId)
     // createTest(dispatch, testname, subject.id);
+    handleClose()
   };
 
   const handleClickOpen = () => {
@@ -69,40 +66,52 @@ export default function TestForm() {
         <ListItemText primary={"Create Test"} />
       </ListItem>
       <Dialog disableBackdropClick disableEscapeKeyDown open={open} onClose={handleClose}>
-
+        <form className={classes.container} id="test-form">
         <DialogTitle>Create a new test</DialogTitle>
-        <form className={classes.container}>
+
           <DialogContent>
               <FormControl className={classes.formControl}>
-                <TextField id="testName" label="Name of the test" />
+                <TextField
+                  required
+                  id="testName"
+                  label="Name of the test"
+                  name="testName"
+                  type="text"
+                  value={testName || ""}
+                  autoComplete="testName"
+                  autoFocus
+                  onChange={(e) => handleTestNameChange(e)}
+                />
               </FormControl>
               <FormControl className={classes.formControl}>
                 <InputLabel id="select-subject-label">Subject</InputLabel>
                 <Select
                   labelId="select-subject-label"
                   id="select-subject"
+                  defaultValue=""
                   value={subject}
-                  onChange={handleChange}
-                  input={<Input />}
+                  onChange={handleInputChange}
+                  input={<Input id="select-subject-label" />}
                 >
                   <MenuItem value="">
                     <em>None</em>
                   </MenuItem>
-                  {subjects.map(({name, id}) => 
-                      <MenuItem key={id} value={name} onChange={(e) => handleInputChange(e)}>{name}</MenuItem>
+                  {subjects.map(({subname, subid}) => 
+                    <MenuItem key={subid} value={subid}>{subname}</MenuItem>
                   )}
                 </Select>
               </FormControl>
           </DialogContent>
+          </form>
           <DialogActions>
             <Button onClick={handleClose} color="primary">
               Cancel
             </Button>
-            <Button onClick={handleClose} color="primary">
+            <Button onClick={handleFormSubmit} color="primary" form="test-form">
               Ok
             </Button>
           </DialogActions>
-        </form>
+        
       </Dialog>
     </div>
   );
