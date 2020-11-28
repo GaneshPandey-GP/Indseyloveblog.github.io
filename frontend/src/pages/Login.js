97c4d216a6fdad8b3fddf0b0ccc8c579ce2e53b7
GaneshPandey-GP <ganeshpandey897@gmail.com>
2020-11-28 19:01:56 +0530

    Login page design

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -3,39 +3,14 @@ import { Link, Redirect } from "react-router-dom";
 import "./Login.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { loginUser, useAuthState } from "../context";
-import {Loading} from "../components/Loading";
+import { Loading } from "../components/Loading";
 import { useForm } from "react-hook-form";
-import {
-  Card,
-  CardContent,
-  Typography,
-  Container,
-  CssBaseline,
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 
-function Login(props) {;
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      "& > *": {
-        margin: theme.spacing(1),
-      },
-    },
-    container: {
-      top: "50%",
-      left: "50%",
-      transform: "translate(-50%, -50%)",
-      position: "absolute",
-    },
-    heading: {
-      paddingTop: "23px",
-    },
-  }));
-  const classes = useStyles();
+function Login(props) {
   const { register, handleSubmit, errors } = useForm();
   const [{ isAuthenticated, loading, errorMessage }, dispatch] = useAuthState();
   const onSubmit = (data, e) => {
-
     e.preventDefault();
     loginUser(dispatch, data);
     console.log(data);
@@ -46,79 +21,77 @@ function Login(props) {;
 
   return (
     <>
-      <Card>
-         <Container component="main" maxWidth="xs" className={classes.container}>
-      <CssBaseline />
-      <Card>
-        <div style={{ backgroundColor: "#ffe" }}>
-          <Typography variant="h4" align="center" className={classes.heading}>
-           Login
-          </Typography>
-          <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
-            <div className="form-group">
-              <label htmlFor="inputForEmail">Email address</label>
-              <span className="mandatory">*</span>
-              <input
-                id="inputForEmail"
-                name="username"
-                autoComplete="off"
-                type="email"
-                className="form-control"
-                aria-describedby="Enter email address"
-                placeholder="Enter email address"
-                ref={register({
-                  required: {
-                    value: true,
-                    message: "Please enter Your Email",
-                  },
-                })}
-              />
-              {errors.username && (
-                <span>
-                  {errors.username.message}
-                </span>
-              )}
-            </div>
-            <div className="form-group">
-              <label htmlFor="inputForPassword">Password</label>
-              <span className="mandatory">*</span>
-              <input
-                type="password"
-                name="password"
-                autoComplete="off"
-                className="form-control"
-                id="inputForPassword"
-                placeholder="Enter password"
-                ref={register({
-                  required: {
-                    value: true,
-                    message: "Please enter password",
-                  },
-                })}
-              />
-              {errors.password && (
-                <span>
-                  {errors.password.message}
-                </span>
-              )}
-            </div>
-            {errorMessage ? <p className="lead text-danger">{errorMessage}</p>: <p></p>}
-            <div className="d-flex align-items-center">
-              <button type="submit" className="btn btn-outline-primary">
-                Login
-              </button>
+      <div className="login-box">
+        <i className="fas fa-user-circle fa-4x head"></i>
 
-              <button className="btn btn-link ml-auto">
-                <Link to="/register">New User</Link>
-              </button>
-            </div>
-          </form>
-          </CardContent>
-        </div>
-      </Card>
-    </Container>
-      </Card>
+        <h1 className="text-center bold">Login</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
+          <div className="form-group textbox">
+            <i class="fas fa-envelope"></i>
+            <input
+              required
+              id="inputForEmail"
+              name="username"
+              autoComplete="off"
+              type="email"
+              className="form-control input"
+              aria-describedby="Enter email address"
+              placeholder="Enter email address"
+              ref={register({
+                required: {
+                  value: true,
+                  message: "Please enter Your Email",
+                },
+              })}
+            />
+
+            {errors.username && (
+              <span className="errorMessage text-capitalize">
+                <ErrorOutlineIcon /> {errors.username.message}
+              </span>
+            )}
+          </div>
+          <div className="form-group textbox">
+            <i class="fas fa-lock"></i>
+            <input
+              type="password"
+              name="password"
+              autoComplete="off"
+              className="form-control input"
+              id="inputForPassword"
+              placeholder="Enter password"
+              ref={register({
+                required: {
+                  value: true,
+                  message: "Please  Enter Your password",
+                },
+              })}
+            />
+
+            {errors.password && (
+              <span className="errorMessage text-capitalize">
+                <ErrorOutlineIcon /> {errors.password.message}
+              </span>
+            )}
+          </div>
+          {errorMessage ? (
+            <p className="lead text-danger">{errorMessage}</p>
+          ) : (
+            <p></p>
+          )}
+          <div className="d-flex align-items-center">
+            <button type="submit" className="btn btn-primary btn">
+              Login
+            </button>
+          </div>
+          <div className="links">
+            <Link to="/"> {"Forgot Password?"}</Link>
+            <br />
+            <Link to="/register">{"Already have an account? Sign In"}</Link>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
--- frontend/src/pages/SubAdminLogin.js
+++ frontend/src/pages/SubAdminLogin.js
@@ -1,134 +1,97 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import {adminlogin, useAuthState} from '../context';
-import {useForm} from 'react-hook-form'
-
-import {
-  Card,
-  CardContent,
-  Typography,
-  Container,
-  CssBaseline,
-} from "@material-ui/core";
+import { adminlogin, useAuthState } from "../context";
+import { useForm } from "react-hook-form";
+import "./Login.css";
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 import { Link, Redirect } from "react-router-dom";
-import {Loading} from "../components/Loading";
+import { Loading } from "../components/Loading";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-    },
-  },
-  container: {
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    position: "absolute",
-  },
-  heading: {
-    paddingTop: "23px",
-  },
-}));
+
 
 export default function SubAdminLogin() {
-    // const [formData, setFormData] = useState({
-  //   username: "",
-  //   password: "",
-  // });
- const{register,handleSubmit,errors} = useForm();
- // const { username, password } = formData;
-
- // const handleInputChange = (e) =>
- //   setFormData({ ...formData, [e.target.name]: e.target.value });
- const [{loading, isAuthenticated, errorMessage}, dispatch] = useAuthState()
- const onSubmit = (data,e) => {
-   e.preventDefault();
-   adminlogin(dispatch, data)
-  console.log(data)
-   // loginaction()
-
- };
+  const { register, handleSubmit, errors } = useForm();
+  const [{ loading, isAuthenticated, errorMessage }, dispatch] = useAuthState();
+  const onSubmit = (data, e) => {
+    e.preventDefault();
+    adminlogin(dispatch, data);
+    console.log(data);
+  };
 
 
-  
+  if (loading) return <Loading />;
+  if (isAuthenticated) return <Redirect to="/sub-admin-dashboard" />;
 
- const classes = useStyles();
-  if(loading) return (<Loading />)
-  if (isAuthenticated)
-    return <Redirect to='/sub-admin-dashboard' />
-
- 
   return (
-    <Container component="main" maxWidth="xs" className={classes.container}>
-      <CssBaseline />
-      <Card>
-        <div style={{ backgroundColor: "#ffe" }}>
-          <Typography variant="h4" align="center" className={classes.heading}>
-            Admin Login
-          </Typography>
-          <CardContent>
-          <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
-            <div className="form-group">
-              <label htmlFor="inputForEmail">Email address</label>
-              <span className="mandatory">*</span>
-              <input
-                id="inputForEmail"
-                name="username"
-                autoComplete="off"
-                type="email"
-                className="form-control"
-                aria-describedby="Enter email address"
-                placeholder="Enter email address"
-                ref={register({
-                  required: {
-                    value: true,
-                    message: "Please enter Your Email",
-                  },
-                })}
-              />
-              {errors.username && (
-                <span>
-                  {errors.username.message}
-                </span>
-              )}
-            </div>
-            <div className="form-group">
-              <label htmlFor="inputForPassword">Password</label>
-              <span className="mandatory">*</span>
-              <input
-                type="password"
-                name="password"
-                autoComplete="off"
-                className="form-control"
-                id="inputForPassword"
-                placeholder="Enter password"
-                ref={register({
-                  required: {
-                    value: true,
-                    message: "Please enter password",
-                  },
-                })}
-              />
-              {errors.password && (
-                <span>
-                  {errors.password.message}
-                </span>
-              )}
-            </div>
-            {errorMessage ? <p className="lead text-danger">{errorMessage}</p>: <p></p>}
-            <div className="d-flex align-items-center">
-              <button type="submit" className="btn btn-outline-primary">
-                Login
-              </button>
+    <div className="login-box">
+      <i className="fas fa-user-circle fa-4x head"></i>
 
-              <button className="btn btn-link ml-auto">
-                <Link to="/register">New User</Link>
-              </button>
-            </div>
-          </form>
-          </CardContent>
+      <h1 className="text-center">Admin Login</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} noValidate autoComplete="off">
+        <div className="form-group textbox">
+          <i class="fas fa-envelope"></i>
+          <input
+            required
+            id="inputForEmail"
+            name="username"
+            autoComplete="off"
+            type="email"
+            className="form-control input"
+            aria-describedby="Enter email address"
+            placeholder="Enter email address"
+            ref={register({
+              required: {
+                value: true,
+                message: "Please enter Your Email",
+              },
+            })}
+          />
+
+          {errors.username && (
+            <span className="errorMessage text-capitalize">
+              <ErrorOutlineIcon /> {errors.username.message}
+            </span>
+          )}
         </div>
-      </Card>
-    </Container>
+        <div className="form-group textbox">
+          <i class="fas fa-lock"></i>
+          <input
+            type="password"
+            name="password"
+            autoComplete="off"
+            className="form-control input"
+            id="inputForPassword"
+            placeholder="Enter password"
+            ref={register({
+              required: {
+                value: true,
+                message: "Please  Enter Your password",
+              },
+            })}
+          />
+
+          {errors.password && (
+            <span className="errorMessage text-capitalize">
+              <ErrorOutlineIcon /> {errors.password.message}
+            </span>
+          )}
+        </div>
+        {errorMessage ? (
+          <p className="lead text-danger">{errorMessage}</p>
+        ) : (
+          <p></p>
+        )}
+        <div className="d-flex align-items-center">
+          <button type="submit" className="btn btn-primary btn">
+            Login
+          </button>
+        </div>
+        <div className="links">
+          <Link to="/"> {"Forgot Password?"}</Link>
+          <br />
+          <Link to="/register">{"Already have an account? Sign In"}</Link>
+        </div>
+      </form>
+    </div>
   );
 }
