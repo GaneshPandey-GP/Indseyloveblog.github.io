e2ebb6f99440c78d6be2b5081e1f850e4ce0cc8d
GaneshPandey-GP <ganeshpandey897@gmail.com>
2020-12-14 08:45:31 +0530

    Timer

--- frontend/src/components/StudentDashboard/Timer.js
+++ frontend/src/components/StudentDashboard/Timer.js
@@ -6,7 +6,7 @@ import TestRedirect from "./TestRedirect";
 export default function Timer(props) {
   const testtime = parseInt(localStorage.getItem("timer"));
   
-  const [minutes, setMinutes] = useState(testtime - 1);
+  const [minutes, setMinutes] = useState(1 - 1);
   const [seconds, setSeconds] = useState(59);
   useEffect(() => {
     localStorage.setItem("min",minutes)
@@ -41,46 +41,13 @@ export default function Timer(props) {
     <>
       {minutes === 0 && seconds === 0 ? (
         <>
-          {/* <div className="alert alert-danger text-center " >
+          <div className="alert alert-danger text-center " >
             Times Up!!
           
-          </div> */}
+          </div> 
          
          
-          <div className="main" role="alert" style={{backdropFilter: 'blur(1115px)', background:"rgba(255,255,255,0.2)",height:"100vh",overflowY:"hidden"}}>
-            <div className tabindex="-1" role="dialog" data-toggle="modal">
-              <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title">Times Up</h5>
-                    <button
-                      type="button"
-                      className="close"
-                      data-dismiss="modal"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                  <div className="modal-body">
-                    <p>Modal body text goes here.</p>
-                  </div>
-                  <div className="modal-footer">
-                    <button type="button" className="btn btn-primary">
-                    <TestRedirect handleSubmit={props.handleSubmit} />
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-secondary"
-                      data-dismiss="modal"
-                    >
-                      Close
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+         
         </>
       ) : (
         <>
